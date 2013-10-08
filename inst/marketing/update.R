f2c81f3a4fb84f26294a600006b9297dfe59a8b6
mostly-harmless <mostly.harmless.71@gmail.com>
2013-10-07 22:48:52 -0700

    update app

--- inst/marketing/update.R
+++ inst/marketing/update.R
@@ -1,65 +1,37 @@
-# This is a wrapper for download.file and has the same interface.
-# The only difference is that, if the protocol is https, it changes the
-# download settings, depending on platform.
-download <- function(url, ...) {
-  # First, check protocol. If http or https, check platform:
-  if (grepl('^https?://', url)) {
-    
-    # If Windows, call setInternet2, then use download.file with defaults.
-    if (.Platform$OS.type == "windows") {
-      # If we directly use setInternet2, R CMD CHECK gives a Note on Mac/Linux
-      mySI2 <- `::`(utils, 'setInternet2')
-      # Store initial settings
-      internet2_start <- mySI2(NA)
-      on.exit(mySI2(internet2_start))
-      
-      # Needed for https
-      mySI2(TRUE)
-      download.file(url, ...)
-      
-    } else {
-      # If non-Windows, check for curl/wget/lynx, then call download.file with
-      # appropriate method.
-      
-      if (nzchar(Sys.which("wget")[1])) {
-        method <- "wget"
-      } else if (nzchar(Sys.which("curl")[1])) {
-        method <- "curl"
-        
-        # curl needs to add a -L option to follow redirects.
-        # Save the original options and restore when we exit.
-        orig_extra_options <- getOption("download.file.extra")
-        on.exit(options(download.file.extra = orig_extra_options))
-        
-        options(download.file.extra = paste("-L", orig_extra_options))
-        
-      } else if (nzchar(Sys.which("lynx")[1])) {
-        method <- "lynx"
-      } else {
-        stop("no download method found")
-      }
-      
-      download.file(url, method = method, ...)
-    }
-    
-  } else {
-    download.file(url, ...)
-  }
+download.file_win <- function(url,f) {
+  download.file(paste0(url,f),f, quiet = TRUE)
 }
 
+download.file_mac <- function(url,f) {
+  download.file(paste0(url,f),f, method = 'curl', quiet = TRUE, extra = '-L')
+}
 
 update_app <- function(url) {
 
-  mtime_file <- 'dbox_remote.rda'
-  try_remote <- try(download(paste0(url,mtime_file),mtime_file), silent = TRUE)
+  os_type <- .Platform$OS.type
+
+  if (os_type == 'windows') {
+    # copied from Shiny source code
+    mySI2 <- `::`(utils, 'setInternet2')
+    internet2_start <- mySI2(NA)
+    on.exit(mySI2(internet2_start))
+    mySI2(TRUE)
+    download <- download.file_win
+  } else { 
+    download <- download.file_mac
+	  # try_remote <- try(download.file(paste0(url,f),f, method = 'curl', quiet = TRUE, extra = '-L'), silent = TRUE)
+  }
+
+  f <- 'dbox_remote.rda'
+  try_remote <- try(download(url,f), silent = TRUE)
 
   if(!is(try_remote, 'try-error')) {
 
     dbox_local <- file.info(list.files(recursive = TRUE, include.dirs = TRUE))
-    load(mtime_file)
+    load(f)
 
     if(dim(dbox_remote)[1] == dim(dbox_local)[1]) {
-      if(dbox_remote$mtime == dbox_local$mtime) stop
+      if(sum(dbox_remote$mtime == dbox_local$mtime) == dim(dbox_local)[1]) stop
     }
 
     # new dirs
@@ -83,7 +55,7 @@ update_app <- function(url) {
     files2get <- c(files2get, existing_files[dbox_remote[existing_files,]$mtime > dbox_local[existing_files,]$mtime])
 
     for(f in files2get) {
-      try(download(paste0(url,f),f), silent = TRUE)
+      download(url,f)
     }
   }
 }
@@ -91,4 +63,3 @@ update_app <- function(url) {
 # url <- 'https://raw.github.com/mostly-harmless/radyant/master/inst/marketing/'
 update_app('https://raw.github.com/mostly-harmless/radyant/master/inst/marketing/')
 shiny::runApp('.')
-

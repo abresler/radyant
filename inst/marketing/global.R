449f27b56920dd641b4aa51de5106c1620967f22
Vincent <vnijs@rady.ucsd.edu>
2013-10-22 20:37:26 -0700

    base_64

--- inst/marketing/global.R
+++ inst/marketing/global.R
@@ -98,7 +98,7 @@ includeRmd <- function(path){
     stop("Markdown package is not installed")
   shiny:::dependsOnFile(path)
   contents <- paste(readLines(path, warn = FALSE), collapse = '\n')
-  html <- knitr::knit2html(text = contents, fragment.only = TRUE, options = "")
+  html <- knitr::knit2html(text = contents, fragment.only = TRUE, options = "base64_images")
   Encoding(html) <- 'UTF-8'
   HTML(html)
 }
